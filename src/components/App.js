66f54028816646f97d42964744db645e96bc2179
Coelacanth7 <galford151@gmail.com>
2018-03-16 00:18:46 -0700

    added some style

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,19 @@ const App = ({ isFetching, error, wikiText }) => {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<h1 className="App-title">Welcome to wikiIpsum</h1>
+				<h1 className="App-title">wiki-ipsum</h1>
+				<h4>
+					a lorem ipsum generator with dynamically generated text from wikipedia
+				</h4>
+				<hr />
 			</header>
-			<p className="App-intro">{wikiText}</p>
+			<article className="textbox">
+				<p className="wikiText">{wikiText}</p>
+			</article>
+
+			<footer>
+				<a href="https://github.com/coelacanth7">Created by coelacanth7</a>
+			</footer>
 		</div>
 	);
 };
